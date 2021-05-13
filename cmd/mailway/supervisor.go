2924c7ccc417a80e59d8199b53388842b761420b
Sven Sauleau <sven@cloudflare.com>
2021-05-13 23:47:51 +0100

    add retry supervisor

--- cmd/mailway/supervisor.go
+++ cmd/mailway/supervisor.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
 	"time"
 
 	"github.com/mailway-app/config"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
 	JWT_CHECK_INTERVAL = 1 * time.Hour
+
+	MAILOUT_RETRY_INTERVAL = 3 * time.Minute
+	MAILWAY_RETRY_SEQ      = []int{-1, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+		233, 377, 610, 987, 1597, 2584, 4181}
 )
 
 func supervise() error {
@@ -22,13 +31,71 @@ func supervise() error {
 			}
 		}()
 	}
+	go func() {
+		if err := superviseMailoutRetrier(); err != nil {
+			log.Fatalf("failed to supervise mailout retrier: %s", err)
+		}
+	}()
 
-	log.Info("supervisor running")
 	<-done
 	return nil
 }
 
+func getNextRetry(modTime time.Time, retry int) (time.Time, error) {
+	if retry > len(MAILWAY_RETRY_SEQ) {
+		return time.Time{}, errors.Errorf("too many retries (%d), ignoring", retry)
+	}
+	n := time.Duration(MAILWAY_RETRY_SEQ[retry]) * 3 * time.Minute
+	return modTime.Add(n), nil
+}
+
+func retryCount(data []byte) int {
+	return strings.Count(string(data), "Mw-Int-Id")
+}
+
+func superviseMailoutRetrier() error {
+	log.Info("mailout retrier running")
+	for range time.Tick(MAILOUT_RETRY_INTERVAL) {
+		files, err := ioutil.ReadDir(config.RUNTIME_LOCATION)
+		if err != nil {
+			return errors.Wrapf(err, "failed to read %s", config.RUNTIME_LOCATION)
+		}
+
+		for _, file := range files {
+			abspath := path.Join(config.RUNTIME_LOCATION, file.Name())
+			data, err := ioutil.ReadFile(abspath)
+			if err != nil {
+				return errors.Wrap(err, "could not read file")
+			}
+
+			retryCount := retryCount(data)
+			nextRetry, err := getNextRetry(file.ModTime(), retryCount)
+			if err != nil {
+				log.Errorf("could not retry: %s", err)
+				continue
+			}
+
+			if nextRetry.Before(time.Now()) {
+				if err := recoverEmail(abspath); err != nil {
+					log.Errorf("failed to recover email: %s", err)
+
+					// delete the email since a new buffer will be created from
+					// the failure
+					if err := os.Remove(abspath); err != nil {
+						return errors.Wrap(err, "could not delete file")
+					}
+				}
+			} else {
+				log.Infof("%s retried %d time(s) next retry in %v",
+					abspath, retryCount, time.Until(nextRetry))
+			}
+		}
+	}
+	return nil
+}
+
 func superviseServerJWT() error {
+	log.Info("server JWT watcher running")
 	for range time.Tick(JWT_CHECK_INTERVAL) {
 		token := config.CurrConfig.ServerJWT
 		if token == "" {
--- go.mod
+++ go.mod
@@ -6,11 +6,11 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/magefile/mage v1.11.0 // indirect
-	github.com/mailway-app/config v0.0.0-20210228114952-43e0fa42e09f
+	github.com/mailway-app/config v0.0.0-20210513211133-2f31c01469d5
 	github.com/manifoldco/promptui v0.8.0
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.8.0
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
-	golang.org/x/sys v0.0.0-20210227040730-b0d1d43c014d // indirect
+	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
 )
